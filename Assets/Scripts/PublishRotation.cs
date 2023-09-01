3edbc40501e073ace741ce424742c3614e473d61
Minhyung Lee <ss49132002@gmail.com>
2023-09-02 00:47:57 +0900

    init: oculus setting feat: publish VR position

---
+++ Assets/Scripts/PublishRotation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PublishRotation : MonoBehaviour
+{
+    private GameObject centerEye;
+
+    private Vector3 headsetPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        centerEye = GameObject.Find("CenterEyeAnchor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        headsetPos = centerEye.transform.position;
+        Debug.Log(headsetPos);
+    }
+}
